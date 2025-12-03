0c8fa1e4cb481cccabc10293af5b3a9de2c07cbf
AmrElsayed00 <131221130+AmrElsayed00@users.noreply.github.com>
2025-12-03 15:26:38 -0800

    Delete Back-end directory

--- Back-end/features/users/routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { registerUser, logUser } from "./handlers.js"; // 🔥 شيل getUsers
-
-//User
-router.post("/user/register", registerUser);
-router.post("/user/login", logUser);
-
-export default router;
