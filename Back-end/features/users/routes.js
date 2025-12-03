cb9952cb24f19c6199bd1b7b881746e7a5249833
aloll <aw3043622@gmail.com>
2025-12-04 00:50:17 +0200

    edit

--- Back-end/features/users/routes.js
+++ Back-end/features/users/routes.js
@@ -1,13 +1,9 @@
 import express from "express";
 const router = express.Router();
-import { registerUser, getUsers, logUser } from "./handlers.js";
+import { registerUser, logUser } from "./handlers.js"; // 🔥 شيل getUsers
 
 //User
 router.post("/user/register", registerUser);
 router.post("/user/login", logUser);
 
-//Admin
-router.get("/user", getUsers);
-
 export default router;
- 
